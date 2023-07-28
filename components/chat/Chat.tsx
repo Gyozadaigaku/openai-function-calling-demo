36b3fe79b5c3f4058bd11cf747bb19b986a24a70
Gyozadaigaku <gaspardcube15@gmail.com>
2023-07-28 20:25:15 +0900

    💄 Add dummy conversation

--- components/chat/Chat.tsx
+++ components/chat/Chat.tsx
@@ -1,8 +1,63 @@
 'use client'
 
-// チャット画面
+import { useState } from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+
+import ChatMessages from '@/components/chat/ChatMessages'
+
+const FormSchema = z.object({
+  prompt: z.string().min(2, {
+    message: '2文字以上入力する必要があります。',
+  }),
+})
+
+export type FormValues = z.infer<typeof FormSchema>
+
+type ChatRole = 'user' | 'assistant'
+
+type ChatMessage = {
+  role: ChatRole
+  content: string
+}
+
+type ChatMessageList = ChatMessage[]
+
 const Chat = () => {
-  return <div>チャット</div>
+  const [messages, setMessages] = useState<ChatMessageList>([
+    {
+      role: 'user',
+      content: 'やあ。',
+    },
+    {
+      role: 'assistant',
+      content: 'やあ。何か手伝おうか？',
+    },
+    {
+      role: 'user',
+      content: 'Hello.',
+    },
+    {
+      role: 'assistant',
+      content: 'Hello！May I help you?',
+    },
+  ])
+
+  const form = useForm<FormValues>({
+    defaultValues: {
+      prompt: '',
+    },
+    resolver: zodResolver(FormSchema),
+  })
+
+  const loading = form.formState.isSubmitting
+
+  return (
+    <>
+      <ChatMessages messages={messages} loading={loading} />
+    </>
+  )
 }
 
 export default Chat
---
+++ components/chat/ChatMessages.tsx
@@ -0,0 +1,55 @@
+'use client'
+
+import Image from 'next/image'
+import { ChatCompletionRequestMessage } from 'openai'
+
+import { cn } from '@/lib/utils'
+import { Skeleton } from '@/components/ui/skeleton'
+
+interface ChatMessagesProps {
+  messages: ChatCompletionRequestMessage[]
+  loading: boolean
+}
+
+const ChatMessages: React.FC<ChatMessagesProps> = ({ messages, loading }) => {
+  return (
+    <div className="space-y-5 pb-40">
+      {messages.map((message, index) => (
+        <div
+          key={index}
+          className={cn(message.role === 'user' && 'flex justify-end')}
+        >
+          <div className="flex items-center">
+            {message.role !== 'user' && (
+              <div className="relative mr-2 h-10 w-10">
+                <Image src="/robot.png" fill alt="robot" />
+              </div>
+            )}
+
+            <div
+              className={cn(
+                'max-w-[500px] p-3 shadow',
+                message.role === 'user'
+                  ? 'rounded-t-lg rounded-bl-lg bg-zinc-100 py-4 text-black dark:bg-zinc-800 dark:text-white'
+                  : 'rounded-t-lg rounded-br-lg bg-primary text-white dark:text-black'
+              )}
+            >
+              <div className="text-sm">{message.content}</div>
+            </div>
+          </div>
+        </div>
+      ))}
+
+      {loading && (
+        <div className="flex items-center">
+          <div className="relative mr-2 h-10 w-10">
+            <Image src="/robot.png" fill alt="robot" />
+          </div>
+          <Skeleton className="h-[44px] w-[300px] rounded-t-lg rounded-br-lg bg-muted" />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default ChatMessages
